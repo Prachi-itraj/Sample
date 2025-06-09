dcbf0de898ae5108c37538500958f40148832fcd
Prachi Itraj <prachiitraj@gmail.com>
2025-06-09 11:23:55 +0200

    Change deploy

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-app=Flask(name)
+
+app = Flask(__name__)  # Initialize the Flask app
 
 @app.route("/")
-def hello()
-	return "Hello from Flask on EC2!"
+def hello():
+    return "Hello from Flask on EC2!"  # Response for the root URL
 
-if name == "main":
-	app.run(host="0.0.0.0",port = 5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # Run the app on all available IPs
+
